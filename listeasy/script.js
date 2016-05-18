4ef3fd6d1aca9723207056b657e885ac527eb312
Jeff Reeve <jeffreyreeve101@gmail.com>
2016-05-18 11:33:11 +0100

    add voice commands

---
+++ listeasy/script.js
@@ -0,0 +1,28 @@
+var template = function(text) {
+  return '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
+};
+
+var main = function() {
+  $('form').submit(function() {
+    return false;  
+  });
+  
+
+var commands ={'add *item' : add,};
+
+var add = function(item)
+{
+  var html = template(item);
+  $('.list').append(html);
+};
+
+annyang.addCommands(commands);
+annyang.start();
+	
+
+
+  
+  
+};
+
+$(document).ready(main);
